0bf5fd9c9440b6b3fb5b8544bf8985fa06d6e83c
Praseetha-KR <voidimagineer@yahoo.com>
2014-10-05 13:10:44 +0530

    Writing tests cont.

--- spec/ColorBoardSpec.js
+++ spec/ColorBoardSpec.js
@@ -1,12 +1,10 @@
-describe("Check var a", function() {
-	var a;
-	it("should be true", function() {
-		a = true;
-		expect(a).toBe(true);
+describe("Checking existance of function hexToRgb", function() {
+	it("Is defined before calling", function() {
+		expect(hexToRgb).toBeDefined();
 	});
 });
-describe("Checking existance of function hexToRgb", function() {
-	it("should be defined before calling", function() {
-		expect(hexToRgb).toBeDefined();
+describe("Checking initial value of hexinput", function() {
+	it("Value is null", function() {
+		expect(hexInput).toBeNull();
 	});
 });
