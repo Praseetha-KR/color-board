4c4f55514678a8a46a8bd69b66d32f259920a0db
Praseetha-KR <voidimagineer@yahoo.com>
2014-10-05 18:22:47 +0530

    modified tests

--- spec/ColorBoardSpec.js
+++ spec/ColorBoardSpec.js
@@ -1,10 +1,28 @@
-describe("Checking existance of function hexToRgb", function() {
-	it("Is defined before calling", function() {
+describe("Function definitions check", function() {
+	it("hexToRgb is defined", function() {
 		expect(hexToRgb).toBeDefined();
 	});
+	it("rgbToHex is defined", function() {
+		expect(rgbToHex).toBeDefined();
+	});
+	it("rgbToHsl is defined", function() {
+		expect(rgbToHsl).toBeDefined();
+	});
+	it("hslToRgb is defined", function() {
+		expect(hslToRgb).toBeDefined();
+	});
+	it("changeColorWithBg is defined", function() {
+		expect(changeColorWithBg).toBeDefined();
+	});
 });
-describe("Checking initial value of hexinput", function() {
-	it("Value is null", function() {
+describe("Empty input fields check", function() {
+	it("hexInput value is null", function() {
 		expect(hexInput).toBeNull();
+	});
+	it("rgbInput value is null", function() {
+		expect(rgbInput).toBeNull();
+	});
+	it("hslInput value is null", function() {
+		expect(hslInput).toBeNull();
 	});
 });
